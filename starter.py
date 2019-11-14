2a94226b6bd3ae1fd8e9dd8f3e18035eca55a8fe
SOUMYA-MURALI <51839332+SOUMYA-MURALI@users.noreply.github.com>
2019-11-14 15:38:50 -0600

    adding new starter.py

--- starter.py
+++ starter.py
@@ -1,3 +1,6 @@
+#Dependencies
+from flask_pymongo import pymongo
+import pandas as pd
 # DATABASE CONNECTION-MONGO DB
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
@@ -7,18 +10,18 @@ mongo = client.UFO
 alien_collection = mongo.db.alien_data
 military_basescollection = mongo.db.military_bases
 sightings_collection = mongo.db.sightings_count
-word_collection = mongo.db.word_count
+word_collection = mongo.db.word_cloud_usatotals
 
 # READING CSV FILES AND CONVERTING TO DATAFRAMES
 alien_data_df = pd.read_csv("data/alien_data.csv")
 military_bases_df = pd.read_csv("data/military_bases.csv")
 sightings_count_df = pd.read_csv("data/sightings_count.csv")
-word_count_df = pd.read_csv("data/word_count.csv")
+word_cloud_df = pd.read_csv("data/word_cloud_usatotals.csv")
 # DATAFRAMES TO DICTIONARIES
 alien = alien_data_df.to_dict(orient = 'records')
 military = military_bases_df.to_dict(orient = 'records')
 sightings = sightings_count_df.to_dict(orient = 'records')
-word = word_count_df.to_dict(orient ='records')
+word = word_cloud_df.to_dict(orient ='records')
 # LOAD DICTIONARIES TO COLLECTIONS
 alien_collection.insert_many(alien)
 military_basescollection.insert_many(military)
